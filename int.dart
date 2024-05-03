32c6e7a8debdf4c0e3d362878cfd4f06faf07372
KingHash23 <markcalvin2003@gmail.com>
2024-05-04 00:22:05 +0300

    arithmetics

--- int.dart
+++ int.dart
@@ -1,8 +1,22 @@
+// program showing initalization and declaration andl also arithmetics
 void main() {
-// intialize a give integer
-  int x, y, sum;
+  // Initialize and declare two integers
+  int x, y, sum, diff, product, ratio;
+
+  // Assign values to the integers
   x = 9;
-  y = x;
+  y = 81;
+
+  // Calculate the sum, difference, product, and ratio of the two integers
   sum = (x + y);
+  diff = (x - y);
+  product = (x * y);
+  ratio = (y ~/
+      x); // Use the ~/ operator to perform integer division and get the integer result
+
+  // Print the results
   print("The sum of $x and $y is $sum");
+  print("The diff of $x and $y is $diff ");
+  print("The product of $x and $y is $product");
+  print("The ratio of $x and $y is $ratio");
 }

0ebc5b2c48b766474d6776af2922532ce2a4b6e5
KingHash23 <markcalvin2003@gmail.com>
2024-05-23 09:02:46 +0300

    if statement

---
+++ ifstatement.dart
@@ -0,0 +1,14 @@
+// a code implementing a dart if statement// This is a simple Dart program that demonstrates the use of an if statement
+
+void main() {
+  // Initialize and declare an integer variable
+  int x;
+
+  // Assign a value to the variable
+  x = 10;
+
+  // Use an if statement to check if the variable is greater than 5
+  if (x > 5) {
+    print("x is greater than 5");
+  }
+}
